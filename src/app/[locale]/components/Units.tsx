44acbacd6247f5be7aa43935a9d2ced01cd5c787
Denny Danuwijaya <denny.danuwijaya@gmail.com>
2023-10-17 03:04:27 +0700

    fix units

--- src/app/[locale]/components/Units.tsx
+++ src/app/[locale]/components/Units.tsx
@@ -1,6 +1,9 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import FsLightbox from "fslightbox-react";
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
+import Image from "next/image";
 
 interface UnitsProps {
   reference: React.MutableRefObject<null>;
@@ -11,8 +14,18 @@ export const Units = ({ reference }: UnitsProps) => {
   const [isMobile, setIsMobile] = useState(false);
   const [selectedImage, setSelectedImage] = useState("");
   const [toggler, setToggler] = useState(false);
+  const [togglerTypicalFloor, setTogglerTypicalFloor] = useState(false);
 
   const tabs = ["Studio", "1 Bedroom", "2 Bedroom", "3 Bedroom"];
+
+  const typicalFloorImages = [
+    {
+      src: "https://res.cloudinary.com/killtdj/image/upload/f_auto/q_auto/v1697484713/TYPICAL_FLOOR_PLAN_TOWER_1_A_1_qa3gvm.jpg",
+    },
+    {
+      src: "https://res.cloudinary.com/killtdj/image/upload/f_auto/q_auto/v1697167105/heartful/siteplan_ioyqjo.png",
+    },
+  ];
 
   const handleTabClick = (tab: string) => {
     setSelectedTab(tab);
@@ -33,11 +46,31 @@ export const Units = ({ reference }: UnitsProps) => {
     setToggler(!toggler);
   };
 
+  const openTypicalFloorLightbox = (imageSrc: React.SetStateAction<string>) => {
+    setSelectedImage(imageSrc);
+    setTogglerTypicalFloor(!togglerTypicalFloor);
+  };
+
   return (
     <div
       className="justify-center items-center flex flex-col space-y-4 p-4 md:p-8 lg:p-12 xl:p-16 2xl:p-24 h-2/3 bg-contour"
       ref={reference}
     >
+      <div className="flex justify-start flex-col items-center">
+        <h1 className="text-xl font-bold text-center text-dark uppercase sm:text-4xl">
+          Our Units
+        </h1>
+        <p className="text-sm text-center my-4">
+          At <strong>Heartful Tower</strong>, we understand that every resident
+          is unique. <br />
+          That's why we offer a variety of unit types to cater to different
+          lifestyles and preferences. <br />
+          No matter which unit you choose, you'll be welcomed into a community{" "}
+          <br />
+          that values quality, comfort, and the joy of urban living.
+        </p>
+      </div>
+
       {selectedImage && selectedTab === "Studio" && (
         <FsLightbox toggler={toggler} sources={[selectedImage]} />
       )}
@@ -53,11 +86,7 @@ export const Units = ({ reference }: UnitsProps) => {
       <div className="bg-tower dark:bg-gray-900 rounded-lg drop-shadow-2xl shadow-gray-700">
         <div className="px-6 pb-12 mx-auto">
           <div className="mt-8 xl:mt-16 lg:flex lg:-mx-12">
-            <div className="lg:mx-12">
-              <h1 className="text-xl font-semibold text-gray-800 dark:text-white">
-                Select Unit
-              </h1>
-
+            <div className="lg:mx-12 flex justify-between flex-col">
               <div className="mt-4 space-y-4 lg:mt-8 flex flex-col items-start">
                 {isMobile ? (
                   <select
@@ -89,6 +118,14 @@ export const Units = ({ reference }: UnitsProps) => {
                   </div>
                 )}
               </div>
+
+              <button
+                onClick={() => setTogglerTypicalFloor(true)}
+                type="button"
+                className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+              >
+                Show Typical Floor
+              </button>
             </div>
 
             <div className="flex-1 mt-8 lg:mx-12 lg:mt-0">
@@ -96,8 +133,10 @@ export const Units = ({ reference }: UnitsProps) => {
                 {selectedTab === "Studio" && (
                   <div className="grid grid-cols-1 gap-8 md:grid-cols-2 xl:grid-cols-2 ">
                     <div>
-                      <img
-                        className="object-cover w-full rounded-lg h-52 sm:h-96"
+                      <Image
+                        width={500}
+                        height={500}
+                        className="object-cover rounded-lg h-52 sm:h-96 cursor-pointer"
                         src="https://res.cloudinary.com/killtdj/image/upload/f_auto/q_auto/v1697167094/heartful/unit/studio_j5ksb6.jpg"
                         alt=""
                         onClick={() =>
@@ -109,8 +148,10 @@ export const Units = ({ reference }: UnitsProps) => {
                     </div>
 
                     <div>
-                      <img
-                        className="object-contain w-full rounded-lg h-52 sm:h-96"
+                      <Image
+                        width={200}
+                        height={200}
+                        className="object-contain w-full rounded-lg h-52 sm:h-96 cursor-pointer"
                         src="https://res.cloudinary.com/killtdj/image/upload/f_auto/q_auto/v1697216861/Unit-Studio-A-21_qrolie.png"
                         alt=""
                         onClick={() =>
@@ -128,8 +169,10 @@ export const Units = ({ reference }: UnitsProps) => {
                 {selectedTab === "1 Bedroom" && (
                   <div className="grid grid-cols-1 gap-8 md:grid-cols-2 xl:grid-cols-2 ">
                     <div>
-                      <img
-                        className="object-cover w-full rounded-lg h-52 sm:h-96"
+                      <Image
+                        width={500}
+                        height={500}
+                        className="object-cover rounded-lg h-52 sm:h-96 cursor-pointer"
                         src="https://res.cloudinary.com/killtdj/image/upload/f_auto/q_auto/v1697167092/heartful/unit/1br_jjf5mx.jpg"
                         alt=""
                         onClick={() =>
@@ -141,8 +184,10 @@ export const Units = ({ reference }: UnitsProps) => {
                     </div>
 
                     <div>
-                      <img
-                        className="object-contain w-full rounded-lg h-52 sm:h-96"
+                      <Image
+                        width={200}
+                        height={200}
+                        className="object-contain w-full rounded-lg h-52 sm:h-96 cursor-pointer"
                         src="https://res.cloudinary.com/killtdj/image/upload/v1697216861/Unit-1-BR-A_nectvc.png"
                         alt=""
                         onClick={() =>
@@ -160,8 +205,10 @@ export const Units = ({ reference }: UnitsProps) => {
                 {selectedTab === "2 Bedroom" && (
                   <div className="grid grid-cols-1 gap-8 md:grid-cols-2 xl:grid-cols-2 ">
                     <div>
-                      <img
-                        className="object-cover w-full rounded-lg h-52 sm:h-96"
+                      <Image
+                        width={500}
+                        height={500}
+                        className="object-cover rounded-lg h-52 sm:h-96 cursor-pointer"
                         src="https://res.cloudinary.com/killtdj/image/upload/f_auto/q_auto/v1697167092/heartful/unit/2br_bdqnbv.jpg"
                         alt=""
                         onClick={() =>
@@ -173,8 +220,10 @@ export const Units = ({ reference }: UnitsProps) => {
                     </div>
 
                     <div>
-                      <img
-                        className="object-contain w-full rounded-lg h-52 sm:h-96"
+                      <Image
+                        width={200}
+                        height={200}
+                        className="object-contain w-full rounded-lg h-52 sm:h-96 cursor-pointer"
                         src="https://res.cloudinary.com/killtdj/image/upload/v1697216861/Unit-2-BR-A-Nett37_ivezfr.png"
                         alt=""
                         onClick={() =>
@@ -190,10 +239,12 @@ export const Units = ({ reference }: UnitsProps) => {
 
               <div>
                 {selectedTab === "3 Bedroom" && (
-                  <div className="grid grid-cols-1 gap-8 md:grid-cols-2 xl:grid-cols-2 ">
+                  <div className="grid grid-cols-1 gap-2 md:grid-cols-2 xl:grid-cols-2 ">
                     <div>
-                      <img
-                        className="object-cover w-full rounded-lg h-52 sm:h-96"
+                      <Image
+                        width={500}
+                        height={500}
+                        className="object-cover rounded-lg cursor-pointer"
                         src="https://res.cloudinary.com/killtdj/image/upload/f_auto/q_auto/v1697167092/heartful/unit/3br_wts5o6.jpg"
                         alt=""
                         onClick={() =>
@@ -205,8 +256,10 @@ export const Units = ({ reference }: UnitsProps) => {
                     </div>
 
                     <div>
-                      <img
-                        className="object-contain w-full rounded-lg h-52 sm:h-96"
+                      <Image
+                        width={200}
+                        height={200}
+                        className="object-contain w-full rounded-lg h-52 sm:h-96 cursor-pointer"
                         src="https://res.cloudinary.com/killtdj/image/upload/f_auto/q_auto/v1697216862/Unit-3-BR_fjrz75.png"
                         alt=""
                         onClick={() =>
@@ -221,14 +274,15 @@ export const Units = ({ reference }: UnitsProps) => {
               </div>
             </div>
           </div>
-          <button
-            type="button"
-            className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
-          >
-            Show Typical Floor
-          </button>
         </div>
       </div>
+      <Lightbox
+        open={togglerTypicalFloor}
+        close={() => setTogglerTypicalFloor(false)}
+        slides={typicalFloorImages}
+        styles={{ container: { backgroundColor: "rgba(0, 0, 0, .8)" } }}
+      />
+      {/*<FsLightbox toggler={togglerTypicalFloor} sources={typicalFloorImages} />*/}
     </div>
   );
 };
