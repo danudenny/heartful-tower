f6c660fc3945ea2ae9c56a95cdb1117ca1d7e752
Marshall Greenfelder <marshallgreenfelder25@outlook.com>
2023-11-29 16:43:24 +0700

    ädd webpack to config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
 const nextConfig = {
+  webpack: (config, { 
+    buildId, 
+    dev, 
+    isServer, 
+    defaultLoaders, 
+    webpack,
+    nextRuntime
+  }) => {
+    // Note: we provide webpack above so you should not `require` it
+    // Perform customizations to webpack config
+    // Important: return the modified config
+    config.plugins.push(
+      new webpack.ProvidePlugin({
+        React: "react",
+      })
+    );
+    return config;
+  },
   reactStrictMode: true,
   images: {
     domains: ["res.cloudinary.com"],
